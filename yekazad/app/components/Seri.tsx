6894e3536f6ebfa7a71cd870f502d6c108ccae68
Cigerxwin İslam <islamtunc748@gmail.com>
2024-07-19 14:21:33 +0300

    ser xere

--- yekazad/app/components/Seri.tsx
+++ yekazad/app/components/Seri.tsx
@@ -20,24 +20,24 @@ function BasicExample() {
             <Nav.Link href="#link"></Nav.Link>
 
             <NavDropdown title="WEB SİTELERİ" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Tanıtım Siteleri</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Blog Siteleri
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">E Ticaret Siteleri</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Sosyal Medya Siteleri
               </NavDropdown.Item>
             </NavDropdown>
 
 
             <NavDropdown title="MOBİL UYGULAMALAR" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Android Uygulamaları</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                İos Uygulamaları  
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3"></NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
