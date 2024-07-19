cd7ebc511122a716cd7e93825e2310c36d7b0eb9
Cigerxwin İslam <islamtunc748@gmail.com>
2024-07-19 10:46:50 +0300

    serxere

--- yekazad/app/layout.tsx
+++ yekazad/app/layout.tsx
@@ -1,7 +1,12 @@
+// Bismillahirahmanirahmanirahim
+
+
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import BasicExample from './components/Seri'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        
+        <BasicExample/>
+        {children}</body>
     </html>
   );
 }
