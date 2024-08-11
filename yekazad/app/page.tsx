de194eda2b032f7ae8dd07578b9c908c72ecf244
Cigerxwin İslam <islamtunc748@gmail.com>
2024-08-11 23:02:48 +0300

    ser xere nu

--- yekazad/app/page.tsx
+++ yekazad/app/page.tsx
@@ -35,10 +35,6 @@ function page() {
 <Container>
 <Alert style={{justifyContent:'center',background:'white',margin:'7px'}}>
   
-<h3>Yakutlar Peyzaj</h3>
-
-<p>Kurumsal hizmet anlayışı ile bahçe peyzaj düzenleme,otomatik sulama sistemi,hazır rulo çim, ve drenaj uygulamaları hizmetlerini vermekteyiz </p>
-
 
 
 
@@ -51,11 +47,10 @@ function page() {
 
 
 <Row>
-<Col> <KitchenSinkExample wene={mmdudu} bttn="daha fazlası .." nav="Çiçekçilik ve Bakım Faaliyetleri" nivis="Bahçenizi güzelleştirmek yakutlar peyzaj ile çok kolay.Herdemyeşil veya ibreli ağaç ve çalılar,yerörtücüler, hazır rulo çim,vb bitki türlerinin temin ve uygulamasını yapıyoruz."/> </Col>
+<Col> <KitchenSinkExample wene={mmdudu} bttn="daha fazlası .." nav="" nivis=" "/> </Col>
 
-<Col> <KitchenSinkExample wene={mmm} bttn="daha fazlası .." nav="Yapısal Peyzaj" nivis="Yapısal peyzaj uygulamaları kapsamında doğal ve prekast elemanlarla zemin döşemesi, duvar kaplaması, araç ve yaya yolları, otoparklar, çocuk oyun alanları, spor alanları gibi fonksiyonların imalatını yapıyoruz."/> </Col>
-<Col> <KitchenSinkExample wene={mmsise} bttn="daha fazlası .." nav="Yapısal Peyzaj" nivis="Yapısal peyzaj uygulamaları kapsamında doğal ve prekast elemanlarla zemin döşemesi, duvar kaplaması, araç ve yaya yolları, otoparklar, çocuk oyun alanları, spor alanları gibi fonksiyonların imalatını yapıyoruz."/> </Col>
-
+<Col> <KitchenSinkExample wene={mmm} bttn="daha fazlası .." nav="" nivis=" "/> </Col>
+<Col> <KitchenSinkExample wene={mmsise} bttn="daha fazlası .." nav="" nivis=""/></Col>
 
 
 </Row>
