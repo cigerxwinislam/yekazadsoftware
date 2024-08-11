661f08c6906306e289937521199d7bfe78e41130
Cigerxwin İslam <islamtunc748@gmail.com>
2024-08-11 22:46:31 +0300

    ser xere nu

--- yekazad/app/components/tegihistin.jsx
+++ yekazad/app/components/tegihistin.jsx
@@ -10,10 +10,10 @@ import { WhatsAppWidget } from 'react-whatsapp-widget';
 export function Tegihistin() {
   return (<div>
     <Alert style={{margin:7}} >
-     <Button  href="tel:+905443404645"><FaPhone  /></Button> Telefon Et
+     <Button  href="tel:+905549765692"><FaPhone  /></Button> Telefon Et
 
 
-  <WhatsAppWidget message=" Whatsapptan Mesajınızı Yazın" inputPlaceHolder="Mesajınızı Yazın" replyTimeText="En kısa zamanda dönüş yapılacaktır" sendButtonText="Gönder" companyName="" phoneNumber="+905443404645" ></WhatsAppWidget>
+  <WhatsAppWidget message=" Whatsapptan Mesajınızı Yazın" inputPlaceHolder="Mesajınızı Yazın" replyTimeText="En kısa zamanda dönüş yapılacaktır" sendButtonText="Gönder" companyName="" phoneNumber="+905549765692" ></WhatsAppWidget>
 
   </Alert>
     </div>
