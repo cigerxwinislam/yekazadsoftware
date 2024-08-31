36fd38d4b9eebac2020ccb6d11f5806d3c99bc85
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-08-31 19:38:59 +0300

    serxere nu

--- yekazad/app/admin/panel/page.tsx
+++ yekazad/app/admin/panel/page.tsx
@@ -2,28 +2,12 @@
 
 
 
-import { useState, useEffect } from 'react'
- 
-function Profile() {
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(true)
- 
-  useEffect(() => {
-    fetch('/api/perwerdehi')
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        setLoading(false)
-      })
-  }, [])
- 
-  if (isLoading) return <p>Loading...</p>
-  if (!data) return <p>No profile data</p>
- 
+import React from 'react'
+
+function page() {
   return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.bio}</p>
-    </div>
+    <div>page</div>
   )
 }
+
+export default page
