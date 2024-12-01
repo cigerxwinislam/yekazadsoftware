cdbde7919d0a86857de39b887a07e8f0f8c33b89
cigerxwinislam <islamtunc748@gmail.com>
2024-12-02 01:51:06 +0300

    serxere nu

---
+++ yekazad/app/malper/admin/sign-up/page.tsx
@@ -0,0 +1,21 @@
+// Bismillahirahmanirahim
+
+
+
+import { SignUp } from '@/page-components/Auth'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      
+
+
+
+
+      <SignUp/>
+    </div>
+  )
+}
+
+export default page
