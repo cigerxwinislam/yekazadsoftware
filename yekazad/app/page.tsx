71eb66a9b28e0ea954b1df6612781a9c4d226ce3
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-12-02 14:16:05 +0300

    Update page.tsx

--- yekazad/app/page.tsx
+++ yekazad/app/page.tsx
@@ -4,11 +4,11 @@
 
 import React from 'react'
 
-import KitchenSinkExample from './card'
+
 
 import Index from '../page-components/Index/Hero'
 import { Alert, Container } from 'react-bootstrap'
-import UncontrolledExample from './wene'
+
 function mmpage() {
   return (
     <div>
