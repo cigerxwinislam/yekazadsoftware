673d7bd7b21eea29cced2c28baf2e94a888529b5
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-12-02 13:42:41 +0300

    Update Login.jsx

--- yekazad/page-components/Auth/Login.jsx
+++ yekazad/page-components/Auth/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
   const router = useRouter();
   useEffect(() => {
     if (isValidating) return;
-    if (user) router.replace('/malper/feed');
+    if (user) router.replace('/malper');
   }, [user, router, isValidating]);
 
   const onSubmit = useCallback(
