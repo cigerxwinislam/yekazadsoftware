92ff0edc27076e37a031a40158ec1a915925d670
cigerxwinislam <islamtunc748@gmail.com>
2024-12-10 23:07:44 +0300

    serxere nu

--- yekazad/components/Layout/index.js
+++ yekazad/components/Layout/index.js
@@ -1,3 +1,7 @@
+// Bismillahirahmanirahim
+
+
+
 export { default as Container } from './Container';
 export { default as Layout } from './Layout';
 export { default as Spacer } from './Spacer';
--- yekazad/components/mmtegihistin.jsx
+++ yekazad/components/mmtegihistin.jsx
@@ -6,28 +6,16 @@ import { MdWhatsapp } from "react-icons/md";
 
 import { Alert, Button } from "react-bootstrap";
 import { FaPhone } from "react-icons/fa";
-import { WhatsAppWidget } from 'react-whatsapp-widget';
 export function Tegihistin() {
   return (<div>
 
 
 
-<div style={{position:"fixed",bottom:"0px"}}>
-    <Alert style={{margin:7,color:"white",background:"gray"}} >
-     <Button  href="tel:905549765692"><FaPhone  /></Button> Merkez <span></span>
-     </Alert>
-
-
-     
-
-
- 
-    </div>
 
 <div style={{position:"fixed",bottom:"0px",right:"0px"}}>
     <Alert style={{margin:7,color:"white",background:"gray"}} >
    
-     <Button  href="tel:905549765692"><FaPhone  /></Button> Ağrı
+     <Button  href="tel:905549765692"><FaPhone  /></Button> Agiri/Turkey
 
      </Alert>
 
